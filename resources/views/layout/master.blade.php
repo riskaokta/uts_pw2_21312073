62398ee57d5925c7066f556249d3eeb7389bd2f1
riskaokta <riska_oktafia@teknokrat.ac.id>
2023-09-08 09:03:16 +0700

    commit lagi

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -27,7 +27,7 @@
     <!-- Brand Logo -->
     <a href="{{asset('admin/index3.html')}}" class="brand-link">
       <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Riska Oktafia</span>
     </a>
 
     <!-- Sidebar -->
--- resources/views/peran/create.blade.php
+++ resources/views/peran/create.blade.php
@@ -31,6 +31,11 @@
     <div class="alert alert-danger">{{ $message }}</div>
     @enderror
 
+    
+    @error('')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+
     <button type="submit" class="btn btn-primary">Simpan</button>
 </form>
 @endsection 
